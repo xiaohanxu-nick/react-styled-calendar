9a584fa363f5cd992026cfde1bf72c488b3a4d06
xiaohanxu <hanxu1996a@gmail.com>
2018-12-28 02:43:56 +0800

    update

--- test/component/DateInput.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'jest-styled-components';
-import renderer from 'react-test-renderer';
-import DateInput from '../component/DateInput';
-
-
-describe('Dateinput', () => {
-  test;
-});
