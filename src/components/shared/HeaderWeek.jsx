b39536e047ceed0a7e61073e1b0733631691b375
xiaohanxu <hanxu1996a@gmail.com>
2019-01-07 16:48:08 +0800

    Renamed and make it as shared component

---
+++ src/components/shared/HeaderWeek.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import dateFns from 'date-fns';
+import PropTypes from 'prop-types';
+import formatWithLocale from '../../helper/formatWithLocale';
+import defaultTheme from '../../defaultTheme';
+import { Col, Row } from './CalendarContainer';
+
+const DaysContainer = styled(Row)`
+  font-weight: 500;
+  font-size: 70%;
+  color: ${({ theme }) => theme.textColorLight};
+  padding: .75em 0;
+  border-bottom: 1px solid ${({ theme }) => theme.borderColor};
+`;
+
+DaysContainer.defaultProps = {
+  theme: defaultTheme,
+};
+
+const HeaderWeek = (props) => {
+  const days = [];
+  const {
+    currentMonth,
+    formatWeek,
+  } = props;
+
+  const startDate = dateFns.startOfWeek(currentMonth);
+
+  for (let i = 0; i < 7; i += 1) {
+    days.push(
+      <Col
+        justifyContent="center"
+        textAlign="center"
+        key={i}
+      >
+        {formatWithLocale(dateFns.addDays(startDate, i), formatWeek)}
+      </Col>,
+    );
+  }
+
+  return <DaysContainer>{days}</DaysContainer>;
+};
+
+HeaderWeek.defaultProps = {
+  currentMonth: new Date(),
+};
+
+HeaderWeek.propTypes = {
+  currentMonth: PropTypes.instanceOf(Date),
+  formatWeek: PropTypes.string.isRequired,
+};
+
+
+export default HeaderWeek;
