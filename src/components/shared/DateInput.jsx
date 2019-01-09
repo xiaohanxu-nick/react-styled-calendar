978e863bd7d78ef16c372c382926a9a5124aae19
xiaohanxu <hanxu1996a@gmail.com>
2019-01-10 01:34:39 +0800

    add the label component

--- src/components/shared/DateInput.jsx
+++ src/components/shared/DateInput.jsx
@@ -18,17 +18,31 @@ const DateInputContainer = styled.div`
   margin-top: 20px;
   margin-bottom: 20px;
   background: transparent;
-
 `;
 
 DateInputContainer.defaultProps = {
   theme: defaultTheme,
 };
 
+const Label = styled.label`
+  color: ${({ theme }) => theme.mainColor};
+  font-size: 18px;
+  font-weight: normal;
+  position: absolute;
+  pointer-evetns: none;
+  top: -15px;
+  transition: 0.2s ease all;
+  opacity: 0.5;
+`;
+
+Label.defaultProps = {
+  theme: defaultTheme,
+};
+
 const HightLight = styled.span`
   position: absolute;
   height: 60%;
-  width: 105px;
+  width: 100%;
   top: 25%;
   left: 0;
   pointer-events: none;
@@ -68,7 +82,7 @@ const InputContainer = styled.input`
   font-size: 18px;
   padding: 10px 0;
   width: 100%;
-  display: block;
+  display:block;
   border:none;
   background: transparent;
   border-radius: 0;
@@ -80,9 +94,11 @@ const InputContainer = styled.input`
     ~ ${Bar}:before, ~${Bar}:after{
       width: 50%;
     }
-
     ~ ${HightLight} {
       animation: ${InputHightLighter} 0.5s ease;
+    }
+    ~ ${Label} {
+      opacity: 1;
     }
   }
 `;
@@ -91,6 +107,7 @@ InputContainer.defaultProps = {
   theme: defaultTheme,
 };
 
+
 const DateInput = (props) => {
   const {
     selectedDate,
@@ -98,6 +115,8 @@ const DateInput = (props) => {
     editting,
     formatDateInput,
     view,
+    withLabel,
+    labelMessage,
   } = props;
   let formattedDate;
 
@@ -118,6 +137,9 @@ const DateInput = (props) => {
       />
       <HightLight />
       <Bar editting={editting} />
+      {
+        withLabel ? <Label>{labelMessage}</Label> : ''
+      }
     </DateInputContainer>
   );
 };
@@ -127,6 +149,8 @@ DateInput.defaultProps = {
   onButtonClick: () => {},
   editting: false,
   formatDateInput: 'YYYY-MM-DD',
+  withLabel: false,
+  labelMessage: 'Date',
 };
 
 DateInput.propTypes = {
@@ -135,5 +159,7 @@ DateInput.propTypes = {
   editting: PropTypes.bool,
   formatDateInput: PropTypes.string,
   view: PropTypes.string.isRequired,
+  withLabel: PropTypes.bool,
+  labelMessage: PropTypes.string,
 };
 export default DateInput;
