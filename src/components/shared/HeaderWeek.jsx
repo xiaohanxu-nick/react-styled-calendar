f02399189cf29051984acab82d619721a232a1aa
xiaohanxu <hanxu1996a@gmail.com>
2019-01-19 16:46:05 +0800

    add the month view and day view

--- src/components/shared/HeaderWeek.jsx
+++ src/components/shared/HeaderWeek.jsx
@@ -20,12 +20,34 @@ DaysContainer.defaultProps = {
   theme: defaultTheme,
 };
 
-const WeekCol = styled.th`
+const WeekDayCol = styled.th`
   height: 2em;
   width: 2em;
+  text-align: center;
+  justify-content: center;
+`;
+
+WeekDayCol.defaultProps = {
+  theme: defaultTheme,
+};
+
+const WeekCol = styled.th`
+  height: 2em;
+  text-align: center;
+  justify-content: center;
 `;
 
 WeekCol.defaultProps = {
+  theme: defaultTheme,
+};
+
+const MonthCol = styled.th`
+  height: 2em;
+  text-align: center;
+  justify-content: center;
+`;
+
+MonthCol.defaultProps = {
   theme: defaultTheme,
 };
 
@@ -34,33 +56,69 @@ const HeaderWeek = (props) => {
   const {
     currentMonth,
     formatWeek,
+    view,
   } = props;
 
-  const startDate = getStartOfWeek(currentMonth);
+  switch (view) {
+    case 'day': {
+      const startDate = getStartOfWeek(currentMonth);
 
-  for (let i = 0; i < 7; i += 1) {
-    days.push(
-      <WeekCol
-        justifyContent="center"
-        textAlign="center"
-        key={i}
-        colSpan="1"
-      >
-        {formatWithLocale(addDay(startDate, i), formatWeek)}
-      </WeekCol>,
-    );
+      for (let i = 0; i < 7; i += 1) {
+        days.push(
+          <WeekDayCol
+            key={i}
+            colSpan="1"
+          >
+            {formatWithLocale(addDay(startDate, i), formatWeek)}
+          </WeekDayCol>,
+        );
+      }
+
+      return <DaysContainer><tr>{days}</tr></DaysContainer>;
+    }
+
+    case 'week': {
+      return (
+        <DaysContainer>
+          <tr>
+            <WeekCol
+              colSpan="7"
+            >
+              Week Number
+            </WeekCol>
+          </tr>
+        </DaysContainer>
+      );
+    }
+    case 'month': {
+      return (
+        <DaysContainer>
+          <tr>
+            <MonthCol
+              colSpan="7"
+            >
+              Month Number
+            </MonthCol>
+          </tr>
+        </DaysContainer>
+      );
+    }
+
+    default: {
+      return '';
+    }
   }
-
-  return <DaysContainer><tr>{days}</tr></DaysContainer>;
 };
 
 HeaderWeek.defaultProps = {
   currentMonth: new Date(),
+  view: 'day',
 };
 
 HeaderWeek.propTypes = {
   currentMonth: PropTypes.instanceOf(Date),
   formatWeek: PropTypes.string.isRequired,
+  view: PropTypes.string.isRequired,
 };
 
 
