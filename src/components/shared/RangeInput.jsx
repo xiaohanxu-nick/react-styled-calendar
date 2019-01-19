f895649721e1c308236dce022a967a87e4395358
xiaohanxu <hanxu1996a@gmail.com>
2019-01-19 16:45:12 +0800

    make the rangeinput responsive for user interaction

--- src/components/shared/RangeInput.jsx
+++ src/components/shared/RangeInput.jsx
@@ -1,21 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import DateInput from './DateInput';
 
 const RangeContainer = styled.div`
 `;
-const RangeInput = ({ toDate, fromDate, onButtonClick }) => (
-  <RangeContainer>
-    <DateInput selectedDate={fromDate} view="day" withLabel labelMessage="From" onButtonClick={onButtonClick} />
-    <DateInput selectedDate={toDate} view="day" withLabel labelMessage="To" onButtonClick={onButtonClick} />
-  </RangeContainer>
-);
+
+class RangeInput extends Component {
+  shouldComponentUpdate(nextProps) {
+    const {
+      whetherFirstInput,
+      fromDate,
+      toDate,
+    } = this.props;
+    if (nextProps.whetherFirstInput !== whetherFirstInput) return true;
+    if (nextProps.fromDate !== fromDate) return true;
+    if (nextProps.toDate !== toDate) return true;
+    return false;
+  }
+
+  componentDidUpdate() {
+    const {
+      whetherFirstInput,
+    } = this.props;
+    if (whetherFirstInput) {
+      this.firstInput.focus();
+    } else {
+      this.secondInput.focus();
+    }
+  }
+
+  setFirstInputRef = (element) => {
+    this.firstInput = element;
+  }
+
+  setSecondInputRef = (element) => {
+    this.secondInput = element;
+  }
+
+  render() {
+    const {
+      toDate,
+      fromDate,
+      onButtonClick,
+      whetherFirstInput,
+      withLabel,
+      fromInputLabel,
+      toInputLabel,
+    } = this.props;
+    return (
+      <RangeContainer>
+        <DateInput
+          setRef={this.setFirstInputRef}
+          selectedDate={fromDate}
+          view="day"
+          withLabel={withLabel}
+          labelMessage={fromInputLabel}
+          onButtonClick={onButtonClick}
+          editting={whetherFirstInput}
+        />
+        <DateInput
+          setRef={this.setSecondInputRef}
+          selectedDate={toDate}
+          view="day"
+          withLabel={withLabel}
+          labelMessage={toInputLabel}
+          onButtonClick={onButtonClick}
+          editting={!whetherFirstInput}
+        />
+      </RangeContainer>
+    );
+  }
+}
+
+RangeInput.defaultProps = {
+  fromInputLabel: 'from',
+  toInputLabel: 'to',
+};
 
 RangeInput.propTypes = {
   toDate: PropTypes.instanceOf(Date).isRequired,
   fromDate: PropTypes.instanceOf(Date).isRequired,
   onButtonClick: PropTypes.func.isRequired,
+  whetherFirstInput: PropTypes.bool.isRequired,
+  withLabel: PropTypes.bool.isRequired,
+  fromInputLabel: PropTypes.string,
+  toInputLabel: PropTypes.string,
 };
 
 export default RangeInput;
